24be702a2db4d34b689e01d2a0680e0fedb3d6ee
Janita <zhuchenjian@huizhi-tech.com>
2017-03-18 11:21:16 +0800

    项目结构

---
+++ elasticsearch-one/src/main/java/com/janita/search/one/SearchOneApplication.java
@@ -0,0 +1,15 @@
+package com.janita.search.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017-03-18 11:15
+ */
+@SpringBootApplication
+public class SearchOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchOneApplication.class,args);
+    }
+}
